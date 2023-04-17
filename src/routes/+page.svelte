2741c247c4e862293f7f6b8af7c7a0ae8defec48
Allan Corbett <allan@intelligentgrowthsolutions.com>
2023-04-17 16:45:38 +0100

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+  <link
+    rel="icon"
+    href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🕹️</text></svg>"
+  />
+  <link rel="stylesheet" href="https://eco.app.igs.farm/dist/eco.css" />
+  <script type="module" src="https://eco.app.igs.farm/dist/eco.esm.js"></script>
+  <script nomodule src="https://eco.app.igs.farm/dist/index.esm.js"></script>
+  <title>Tetrigs</title>
+</svelte:head>
+
+<eco-layout-body>
+  <eco-layout-nav slot="layout-nav" app-name="🕹️ Tetrigs" />
+  <eco-tetrigs />
+</eco-layout-body>
+
+<style>
+</style>
